b15f3cfc07feece8abe9451bb052a8ad29194768
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-09-28 01:42:10 +0530

    Refactor code for printing right-angled triangle pattern

---
+++ patterns/rightangledtrainglepattern/rightpattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void main(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        main(n);
+    }
+    return 0;
+}
