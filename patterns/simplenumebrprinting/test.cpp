8830bb7fd2aa10ea97772f8b7f58beabcf259a80
prakharsingh-74 <educationisfreedom.75938@gmail.com>
2025-08-26 17:23:36 +0530

    siimple pattern printing

--- patterns/simplenumebrprinting/test.cpp
+++ patterns/simplenumebrprinting/test.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main(){
     int row, col;
-    for(row=1; row=5; row=row+1){
-        for (col=1; col<=5; col=col+1){
+    for(row=1; row<=5; row++){
+        for (col=1; col<=5; col++){
             cout << row << "";
         }
         cout << endl;
     }
+    return 0;
 }
 
 /*
