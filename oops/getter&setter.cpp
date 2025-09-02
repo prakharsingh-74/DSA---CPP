0f081e8a4c4d903ce59d332249a28c268dd09533
prakharsingh-74 <educationisfreedom.75938@gmail.com>
2025-09-02 20:35:07 +0530

    2nd stage commit

---
+++ oops/getter&setter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+    private:
+        double salary;
+    
+    public:
+        string name;
+        string dept;
+        string subject;
+
+        void changeDept(string newDept){
+            dept = newDept;
+        }
+
+        //setter
+        void setSalary(double s){
+            salary=s;
+        }
+
+        //getter
+        double getSalary(){
+            return salary;
+        }
+};
+
+int main(){
+    Teacher t1;
+    t1.name = "Prakhar";
+    t1.subject = "C++";
+    t1.dept = "CSE";
+    t1.setSalary(25000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;   
+}
