e464d7d4406eaa84ce842eb3b5b08e5086ad4e1f
prakharsingh-74 <educationisfreedom.75938@gmail.com>
2025-09-03 00:44:26 +0530

    non-para constructor

---
+++ oops/constructornon-parametrized.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+    private:
+        double salary;
+    
+    public:
+        //non-parametrized constructor
+        Teacher(){
+            dept = "computer science";
+        }//custom constructor, if you dont create a constructor then compiler will create it by default when you run the program
+        
+        string name;
+        string dept;
+        string subject;
+
+        void changeDept(string newDept){
+            dept = newDept;
+        }
+
+        //setter
+        void setSalary(double s){
+            salary=s;
+        }
+
+        //getter
+        double getSalary(){
+            return salary;
+        }
+};
+
+class Account{
+private:
+    double balance;
+    string password; //data hiding 
+public:
+    string accountId;
+    string username;
+};
+
+int main(){
+    Teacher t1;//constructor call
+    t1.name = "Prakhar";
+    t1.subject = "C++";
+    t1.dept = "CSE";
+    t1.setSalary(25000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;   
+}
